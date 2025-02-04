2ef6c49c2afe6dad2e24662141d1f959237bf978
Sivamalraj <sivaa9554@gmail.com>
2025-02-04 10:57:49 +0530

    Add files via upload

---
+++ MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyComponent = () => (
+  <View>
+    <Text>This is a Cloud Component from GitHub!</Text>
+  </View>
+);
+
+export default MyComponent;
