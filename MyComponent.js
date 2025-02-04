72b67b1b6afba2e539f7907c6e61d66208077f00
Sivamalraj <sivaa9554@gmail.com>
2025-02-04 11:24:46 +0530

    MyComponent.js

--- MyComponent.js
+++ MyComponent.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-const MyComponent = () => (
-  <View>
-    <Text>This is a Cloud Component from GitHub!</Text>
-  </View>
-);
-
-export default MyComponent;
+export default function MyComponent() {
+  return (
+    <View>
+      <Text>This is a Cloud Component from GitHub!</Text>
+    </View>
+  );
+}
